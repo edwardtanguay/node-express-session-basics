dd4d1a6e8509c4ad1a84a6f849a3454738b6fbad
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-11 15:59:53 +0200

    installed nodemon

--- server.js
+++ server.js
@@ -10,52 +10,53 @@ const PORT = 3003;
 
 app.use(cookieParser());
 app.use(
-	session({
-		resave: true,
-		saveUninitialized: true,
-		secret: process.env.SESSION_SECRET
-	})
+    session({
+        resave: true,
+        saveUninitialized: true,
+        secret: process.env.SESSION_SECRET
+    })
 );
 
 const users = [
-	{
-		username: "ja",
-		firstName: "James",
-		lastName: "Anderson",
-		email: "ja@mail.com"
-	},
-	{
-		username: "ac",
-		firstName: "Ashley",
-		lastName: "Cartwright",
-		email: "ac@mail.com"
-	}
+    {
+        username: 'ja',
+        firstName: 'James',
+        lastName: 'Anderson',
+        email: 'ja@mail.com'
+    },
+    {
+        username: 'ac',
+        firstName: 'Ashley',
+        lastName: 'Cartwright',
+        email: 'ac@mail.com'
+    }
 ];
 
-app.get("/login/:username", (req, res) => {
-	const user = users.find(user => user.username === req.params.username);
-	if (user) {
-		req.session.user = user;
-		req.session.save();
-		res.send(`User logged in: ${JSON.stringify(user)}`);
-	} else {
-		res.status(500).send("bad login");
-	}
+app.get('/login/:username', (req, res) => {
+    const user = users.find((user) => user.username === req.params.username);
+    if (user) {
+        req.session.user = user;
+        req.session.save();
+        res.send(`User logged in: ${JSON.stringify(user)}`);
+    } else {
+        res.status(500).send('bad login');
+    }
 });
 
-app.get("/user", (req, res) => {
-	if (req.session.user) {
-		res.send(req.session.user);
-	} else {
-		res.send('no user logged in');
-	}
+app.get('/user', (req, res) => {
+    if (req.session.user) {
+        res.send(req.session.user);
+    } else {
+        res.send('no user logged in');
+    }
 });
 
-app.get("/logout", (req, res) => {
-	req.session.destroy();
-	res.send("User logged out");
+app.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        res.send('User logged out');
+    });
 });
 
 app.listen(PORT, (req, res) => {
-	console.log(`API listening on port ${PORT}`);
+    console.log(`API listening on port ${PORT}`);
 });
